85e551e6fc4bcc4503b1fb65dc9dd1fcdf43f03e
Dhruuv Khurana <dhruuv.khurana@ttu.edu>
2017-05-08 02:58:37 -0500

    new_siftr
    stylesheets still not working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	has_many :posts
 	validates :password, presence: true, length: {minimum: 8}
 	validates :username, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
   resources :posts do
   	resources :comments
   end
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  root 'pages#home'
 end
